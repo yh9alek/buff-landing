a53bd17768f69f8fd8e9d4cbf17a06713d207f61
Yohan Alek <yh9dev@gmail.com>
2023-10-13 20:45:42 -0700

    The search form was added in the header

--- app.js
+++ app.js
@@ -10,3 +10,11 @@ window.onscroll = () => {
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 }
+
+document.querySelector('#search-icon').onclick = () => {
+  document.querySelector('#search-form').classList.toggle('active');
+}
+
+document.querySelector('#close').onclick = () => {
+  document.querySelector('#search-form').classList.toggle('active');
+}
