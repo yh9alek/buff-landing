e1ff3de445e5d056a253e8d3fe829df3369b1116
Yohan Alek <yh9dev@gmail.com>
2023-10-13 20:53:07 -0700

    The home section was finished with responsive design, also was configured swiper.js to make a slide

--- app.js
+++ app.js
@@ -18,3 +18,18 @@ document.querySelector('#search-icon').onclick = () => {
 document.querySelector('#close').onclick = () => {
   document.querySelector('#search-form').classList.toggle('active');
 }
+
+//swiper script
+const swiper = new Swiper(".home-slider", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop: true,
+});
