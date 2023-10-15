5b6c31cd41720e6a603c9b6912dd146988442b9d
Yohan Alek <yh9dev@gmail.com>
2023-10-14 18:42:53 -0700

    The scroll was configured in Javascript to activate the respective nav links when the user scrolls down in the different sections

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 let menu = document.querySelector('#menu-bars');
 let navbar = document.querySelector('.navbar');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
 
 menu.onclick = () => {
     menu.classList.toggle('fa-times');
@@ -9,6 +11,18 @@ menu.onclick = () => {
 window.onscroll = () => {
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let height = sec.offsetHeight;
+        let offset = sec.offsetTop - 150;
+        let id = sec.getAttribute('id');
+        if(top >= offset && top < offset + height) {
+          navLinks.forEach(link => {
+            link.classList.remove('active');
+          });
+          document.querySelector('header .navbar a[href*=' + id + ']').classList.add('active');
+        }
+    });
 }
 
 document.querySelector('#search-icon').onclick = () => {
